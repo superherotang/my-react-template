2cede6237f335b257dce3b890c67bb638fbb15d9
alectang <445595661@qq.com>
2022-09-17 15:15:32 +0800

    base

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -5,6 +5,10 @@ const CopyPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
+const globAll = require("glob-all");
+const { PurgeCSSPlugin } = require("purgecss-webpack-plugin");
+const glob = require("glob");
+const CompressionPlugin = require("compression-webpack-plugin");
 
 module.exports = merge(baseConfig, {
   mode: "production", // 生产模式,会开启tree-shaking和压缩代码,以及其他优化
@@ -21,9 +25,22 @@ module.exports = merge(baseConfig, {
         },
       ],
     }),
-    // 抽离css插件
+    /// 抽离css插件
     new MiniCssExtractPlugin({
-      filename: "static/css/[name].[contenthash:8].css", // 抽离css的输出目录和名称
+      filename: "static/css/[name].[contenthash:8].css",
+    }),
+    // 清理无用css
+    new PurgeCSSPlugin({
+      // 检测src下所有tsx文件和public下index.html中使用的类名和id和标签名称
+      // 只打包这些文件中用到的样式
+      paths: globAll.sync([
+        `${path.join(__dirname, "../src")}/**/*.tsx`,
+        path.join(__dirname, "../public/index.html"),
+      ]),
+      //白名单
+      // safelist: {
+      //   standard: [/^ant-/], // 过滤以ant-开头的类名，哪怕没用到也不删除
+      // }
     }),
     new TerserPlugin({
       // 压缩js
@@ -33,6 +50,14 @@ module.exports = merge(baseConfig, {
           pure_funcs: ["console.log"], // 删除console.log
         },
       },
+    }),
+    new CompressionPlugin({
+      test: /.(js|css)$/, // 只生成css,js压缩文件
+      filename: "[path][base].gz", // 文件命名
+      algorithm: "gzip", // 压缩格式,默认是gzip
+      test: /.(js|css)$/, // 只生成css,js压缩文件
+      threshold: 10240, // 只有大小大于该值的资源会被处理。默认值是 10k
+      minRatio: 0.8, // 压缩率,默认值是 0.8
     }),
   ],
   optimization: {
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
-import React from 'react'
-import smallImg from '@/assets/imgs/5kb.png'
-import bigImg from '@/assets/imgs/22kb.png'
-import '@/app.css'
+import React, { lazy, Suspense, useState } from 'react'
+
+// prefetch
+const PreFetchDemo = lazy(() => import(
+  /* webpackChunkName: "PreFetchDemo" */
+  /*webpackPrefetch: true*/
+  '@/components/PreFetchDemo'
+))
+// preload
+const PreloadDemo = lazy(() => import(
+  /* webpackChunkName: "PreloadDemo" */
+  /*webpackPreload: true*/
+  '@/components/PreloadDemo'
+ ))
 
 function App() {
+  const [ show, setShow ] = useState(false)
+
+  const onClick = () => {
+    setShow(true)
+  }
   return (
     <>
-      <img src={smallImg} alt="小于10kb的图片" />
-      <img src={bigImg} alt="大于于10kb的图片" />
-      <div className='smallImg'></div> {/* 小图片背景容器 */}
-      <div className='bigImg'></div> {/* 大图片背景容器 */}
+      <h2 onClick={onClick}>展示</h2>
+      {/* show为true时加载组件 */}
+      { show && (
+        <>
+          <Suspense fallback={null}><PreloadDemo /></Suspense>
+          <Suspense fallback={null}><PreFetchDemo /></Suspense>
+        </>
+      ) }
     </>
   )
 }

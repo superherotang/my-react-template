2f4b1542a6b611e266242f7daf12e085be3ddeb7
alectang <445595661@qq.com>
2022-09-17 13:29:45 +0800

    images

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
-import './app.css'
+import React from "react";
+import smallImg from "./assets/imgs/5kb.png";
+import bigImg from "./assets/imgs/22kb.png";
+import "./app.css";
 
 function App() {
-  return <h2>webpack5-rea11ct-ts</h2>
+  return (
+    <>
+      <img src={smallImg} alt="小于10kb的图片" />
+      <img src={bigImg} alt="大于于10kb的图片" />
+      <div className="smallImg"></div> {/* 小图片背景容器 */}
+      <div className="bigImg"></div> {/* 大图片背景容器 */}
+    </>
+  );
 }
-export default App
+export default App;

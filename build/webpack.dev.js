9fbae1a0576aedc708c9999e0b4f9800a392aebc
alectang <445595661@qq.com>
2022-09-17 14:07:38 +0800

    热更新

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,11 +1,12 @@
-const path = require('path')
-const { merge } = require('webpack-merge')
-const baseConfig = require('./webpack.base.js')
+const path = require("path");
+const { merge } = require("webpack-merge");
+const baseConfig = require("./webpack.base.js");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
 // 合并公共配置,并添加开发环境配置
 module.exports = merge(baseConfig, {
-  mode: 'development', // 开发模式,打包更加快速,省了代码优化步骤
-  devtool: 'eval-cheap-module-source-map', // 源码调试模式,后面会讲
+  mode: "development", // 开发模式,打包更加快速,省了代码优化步骤
+  devtool: "eval-cheap-module-source-map", // 源码调试模式,后面会讲
   devServer: {
     port: 3000, // 服务端口号
     compress: false, // gzip压缩,开发环境不开启,提升热更新速度
@@ -13,6 +14,9 @@ module.exports = merge(baseConfig, {
     historyApiFallback: true, // 解决history路由404问题
     static: {
       directory: path.join(__dirname, "../public"), //托管静态资源public文件夹
-    }
-  }
-})
+    },
+  },
+  plugins: [
+    new ReactRefreshWebpackPlugin(), // 添加热更新插件
+  ],
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import React from "react";
-import smallImg from "./assets/imgs/5kb.png";
-import bigImg from "./assets/imgs/22kb.png";
-import "./app.css";
+import React, { useState } from 'react'
 
 function App() {
+  const [ count, setCounts ] = useState('')
+  const onChange = (e: any) => {
+    setCounts(e.target.value)
+  }
   return (
     <>
-      <img src={smallImg} alt="小于10kb的图片" />
-      <img src={bigImg} alt="大于于10kb的图片" />
-      <div className="smallImg"></div> {/* 小图片背景容器 */}
-      <div className="bigImg"></div> {/* 大图片背景容器 */}
+      <h2>更改测试</h2>
+      <p>受控组件</p>
+      <input type="text" value={count} onChange={onChange} />
+      <br />
+      <p>非受控组件</p>
+      <input type="text" />
     </>
-  );
+  )
 }
-export default App;
+export default App
